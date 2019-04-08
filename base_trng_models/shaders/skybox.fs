4a6e4bc69e969f753215b43912679407b7d4c546
Spivak Andrey <amspivak@mail.ru>
2019-04-08 15:58:40 +0300

    blue sky

--- base_trng_models/shaders/skybox.fs
+++ base_trng_models/shaders/skybox.fs
@@ -9,7 +9,12 @@ void main()
 {
 	
 	vec4 Color = texture(skybox, TexCoords);
+	float l=smoothstep(0.0,0.3, TexCoords.y);
+	// l*=0.0;
+	vec3 atmosphere = vec3(0.9,0.9,1.0);
+	vec3 sky = vec3(0.0,0.6,1.0);
 	//FragColor = vec4(TexCoords.xyz,1.0);
-	FragColor = vec4(Color.xyz,1.0);
+	//FragColor = vec4(Color.xyz,1.0);
+	FragColor = vec4(atmosphere*(1.0-l)+sky*l,1.0);
 
 }
--- base_trng_models/shaders/sobel_aa.fs
+++ base_trng_models/shaders/sobel_aa.fs
@@ -114,8 +114,8 @@ void main()
 	blur =  intens/(intens+0.01);
 	float l=smoothstep(0.999,1.0, d);
 	l*=0.25;
-	//l*=0.0;
-	vec3 atmosphere = vec3(0.99,0.99,1.0);
+	// l*=0.0;
+	vec3 atmosphere = vec3(0.9,0.9,1.0);
 
 	FragColor = vec4((blur)*(texColor.xyz*(1.0 - l)+l*atmosphere), 1.0);
 	//FragColor = vec4(l,l,l, 1.0);
