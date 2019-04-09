223053d057955c020c487ab75f3a8b759ce4c142
Spivak Andrey <amspivak@mail.ru>
2019-04-09 16:15:32 +0300

    sun spot

--- base_trng_models/shaders/skybox.fs
+++ base_trng_models/shaders/skybox.fs
@@ -18,10 +18,12 @@ void main()
 	//vec3 sun = vec3(5.9,0.0,0.0);
 	vec3 sun_dir = LightDir;
 	float to_sun = dot(normalize(TexCoords.xyz),normalize(sun_dir));
-	float sun_l = smoothstep(0.7,1.0,to_sun) *0.2;
+	float sun_l = smoothstep(0.95,1.0,to_sun) *0.1;
 	//float sun_l = smoothstep(0.5,1.0,dot(normalize(TexCoords.xyz),normalize(sun_dir)));
 	vec3 atmosphere = vec3(0.9,0.9,1.0);
 	vec3 sky = vec3(0.0,0.6,1.0) *(1.0 -sun_l) + sun_l*sun;
+	sun_l = smoothstep(0.9995,1.0,to_sun) *0.8;
+	sky = sky *(1.0 -sun_l) + sun_l*sun;
 	//FragColor = vec4(TexCoords.xyz,1.0);
 	//FragColor = vec4(Color.xyz,1.0);
 	FragColor = vec4(atmosphere*(1.0-l)+sky*l,1.0);
